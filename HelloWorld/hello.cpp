1cff9ea7ab4a5f9dd7065b3eecbddd9569436e83
brower <brower@bu.edu>
2023-09-04 23:10:35 -0400

    CCS Hello World Start

---
+++ HelloWorld/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+   printf("\n Hello World! \n Not so good ? \n");
+   cout << " Too bad for you ! " << endl;
+   return 0;
+}
